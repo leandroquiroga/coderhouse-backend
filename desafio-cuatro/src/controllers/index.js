1707b139ec8c924e65b52a2870719ad77e5202c3
leequiro <leandroquiroga9514@gmail.com>
2022-10-25 09:21:41 -0300

    config: Has been added new routes

--- desafio-cuatro/src/controllers/index.js
+++ desafio-cuatro/src/controllers/index.js
@@ -1,181 +1,191 @@
-const fs = require('fs')
-const { response, request } = require('express');
+import { response, request } from 'express';
+import { producto } from '../../app.js';
+
 
 // Obtenemos TODOS los productos
-const getAllProducts = async(_req = request, res = response) => {
-
+export const getAllProducts = async(_req = request, res = response) => {
   try {
-    const prodcutos = await fs.promises.readFile('./src/data/products.json', 'utf-8');
-    const arrayProductos = JSON.parse(prodcutos);
-    
+    // Obtenemos los productos
+    const productos = await producto.getAllProducts();
     // Preguntamos si hay productos
-    if(arrayProductos.length === 0){
+    if(productos.length === 0){
       return res.status(400).json({
         mensaje: 'No hay productos disponibles, por favor ingrese nuevos productos',
+        status: false
       });
     };
-  
-    
 
     res.status(200).json({
+      status: true,
       mensaje: 'Retorna todos los productos',
-      arrayProductos
+      productos
     });
     
   } catch (error) {
     res.status(500).json({
       mensaje: 'Ups! Ha ocurrido un error, por favor consulte con el administrador',
-      error: error.message
+      status: false
     });
   };
 
 };
 
 // Obtenemos un producto por ID
-const getProductsByID = async(req = request, res = response) => {
+export const getProductsByID = async(req = request, res = response) => {
   const id  = Number(req.params.id);
-
   try {
-    const prodcutos = await fs.promises.readFile('./src/data/products.json', 'utf-8');
-      
-    const arrayProductos = JSON.parse(prodcutos);
-    
-    // Buscamos el producto por ID
-    const producto = arrayProductos.find( producto => producto.id === id);
-  
+    const productoFiltrado = await producto.filterProduct(id);
+
     // Preguntamos si existe el producto
-    if(!producto){
+    if(productoFiltrado.length === 0){
       return res.status(400).json({
-        mensaje: `No existe el producto con el ID: ${id}`
+        mensaje: `No existe el producto con el ID: ${id}`,
+        status: false
       });
     };
   
     // Si existe el producto entonces mostromos la coincidencia
     res.status(200).json({
+      status: true,
       mensaje: 'Retorna un prodcuto por id: ',
-      producto
+      productoFiltrado
     });
     
   } catch (error) {
     res.status(500).json({
       mensaje: 'Ups! Ha ocurrido un error, por favor consulte con el administrador',
-      error: error.message
+      status: false
     });
   }
 };
 
-//TODO: Realizar el POST
-const postNewProduct = (req = request, res = response) => {
-  const data = req.body;
 
-  res.send
-  res.status(201).json({
-    mensaje: 'Se ha creado un nuevo producto', 
-    data
-  });
+export const postNewProduct = async(req = request, res = response) => {
+  
+  try {
+    const data = req.body;
+    const {name, price, url_imagen} = data;
+
+    if(!name){
+      return res.status(400).json({
+        status: false,
+        mensaje: 'El campo nombre es obligatorio'
+      });
+    };
+
+    if(!price){
+      return res.status(400).json({
+        status: false,
+        mensaje: 'El campo precio es obligatorio'
+      });
+    };
+
+    if(!url_imagen){
+      return res.status(400).json({
+        status: false,
+        mensaje: 'El campo URL es obligatorio'
+      });
+    };
+
+
+    if(!data){
+      return res.status(400).json({
+        mensaje: 'Los campos son obligatorios'
+      });
+    };
+
+   const info = await producto.newProduct(data);
+
+    res.status(200).json({
+      status: true,
+      mensaje: 'Se ha creado un nuevo producto', 
+      info
+    });
+    
+  } catch (error) {
+    res.status(500).json({
+      status: false,
+      mensaje: 'Ups! Comuniquese con el administrador'
+    });
+  }
 };
 
 // Editamos un producto por ID 
-const putProductByID = async(req = request, res = response) => {
+export const putProductByID = async(req = request, res = response) => {
   const id  = Number(req.params.id);
-  const {price, title} = req.body;
+  const {price, name} = req.body;
 
   try {
-    const productos = await fs.promises.readFile('./src/data/products.json', 'utf-8');
-    
-    const arrayProductos = JSON.parse(productos);
-
-    // Buscamos el producto por ID
-    const existeProducto = arrayProductos.find( producto => producto.id === id);
+    const productoFiltrado =  await producto.filterProduct(id);
   
     // Preguntamos si existe el producto
-    if(!existeProducto){
+    if(!productoFiltrado){
       return res.status(400).json({
-        mensaje: `No existe el producto con el ID: ${id}`
+        mensaje: `No existe el producto con el ID: ${id}`,
+        status: false
       });
     };
   
     // Preguntamos si el precio existe
     if(!price){
       return res.status(400).json({
-        mensaje: `Se debe colocar un precio`
+        mensaje: `Se debe colocar un precio`,
+        status: false
       });
     };
 
     // Preguntamos si el nombre del existe
-    if(!title){
+    if(!name){
       return res.status(400).json({
-        mensaje: `Se debe colocar un titulo`
+        mensaje: `Se debe colocar un titulo`,
+        status: false
       });
     };
 
+    const prodcutoActualizado = { name, price };
+    await producto.putProducts(id, prodcutoActualizado)  
 
-    // Actualizamos Productos
-    arrayProductos.forEach(prod => {
-      if(prod.id === id){
-        prod.id = id;
-        prod.price = price,
-        prod.title = title,
-        prod.image = producto.image
-      };
-    });
-
-    // Sobrescribimos los producutos acutualzados
-    await fs.promises.writeFile('./src/data/products.json', JSON.stringify(arrayProductos));
-  
     res.status(200).json({
       mensaje: 'Se ha actualziado un nuevo producto', 
+      status: true
     });
   } catch (error) {
     res.status(500).json({
       mensaje: 'Ups! Ha ocurrido un error, por favor consulte con el administrador',
-      error: error.message
+      status: false
     });
   };
 
 };
 
 // Eliminamos un producto
-const deleteProduct = async(req = request, res = response ) => {
+export const deleteProduct = async(req = request, res = response ) => {
   const id = Number(req.params.id);
 
   try {
-    const productos = await fs.promises.readFile('./src/data/products.json', 'utf-8');
-    
-    const arrayProductos = JSON.parse(productos);
-
+    // const productos = await prodcuto.getAllProducts();
     // Buscamos el producto por ID
-    const existeProducto = arrayProductos.find( producto => producto.id === id);
-  
+    const existeProducto = await producto.filterProduct(id);
+
     // Preguntamos si existe el producto
-    if(!existeProducto){
+    if(existeProducto.length === 0){
       return res.status(400).json({
         mensaje: `No existe el producto con el ID: ${id}`
       });
     };
-
-    const nuevoArrayProducto = arrayProductos.filter(producto => producto.id !== id);
-
-    await fs.promises.writeFile('./src/data/products.json', JSON.stringify(nuevoArrayProducto));
-
+    // Eliminamos el producto
+    await producto.deleteProduct(id);
 
     res.status(200).json({
-      mensaje: `Se ha eliminado un producto con ID ${id}`
+      mensaje: `Se ha eliminado un producto con ID ${id}`,
+      status: true
     });
 
   } catch (error) {
     res.status(500).json({
       mensaje: 'Ups! Ha ocurrido un error, por favor consulte con el administrador',
-      error: error.message
+      status: false
     });
   };
 
 };
-
-module.exports = {
-  getAllProducts,
-  getProductsByID,
-  postNewProduct,
-  putProductByID,
-  deleteProduct,
-}
