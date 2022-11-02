c0c103489d87127c2b5e436af6a4edf8dc0cc3b7
leandro <leandroquiroga9514@gmail.com>
2022-11-02 14:52:24 -0300

    router: Router of the application

--- desafio-cinco/src/routes/index.js
+++ desafio-cinco/src/routes/index.js
@@ -1,5 +1,4 @@
 import { Router } from 'express';
- 
 import { pageInit } from '../controllers/init_controllers.js';
 import { postNewProduct } from '../controllers/products_controllers.js';
 import { getAllProducts } from '../controllers/allProducts_controller.js';
