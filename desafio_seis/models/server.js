91c9a6c636365cba06afa2f7b5e5ce17221a155d
leandroquiroga <leandroquiroga9514@gmail.com>
2022-11-05 21:09:27 -0300

    fix: Has been changed type from module to commonjs and added new method

--- desafio_seis/models/server.js
+++ desafio_seis/models/server.js
@@ -1,10 +1,13 @@
-import { createServer } from 'http';
-import express from "express";
-import cors from 'cors';
-import { Server as Socket } from 'socket.io';
+const fs = require('fs');
+const path = require('path');
+const { createServer } = require('http');
 
-import router from '../routes/index.js';
+const express = require('express');
+const cors = require('cors');
+const { Server: Socket } = require('socket.io')
 
+const router = require('../routes/index.js');
+const productos = require('../data/productos.json')
 class Server {
   
   constructor() {
@@ -13,17 +16,22 @@ class Server {
     this.server = createServer(this.app);
     this.io     = new Socket(this.server);
     
-
     this.socket();
     this.middleware();
     this.router();
+    this.templates()
   };
 
   middleware() {
-    this.app.use(express.static('public'));
     this.app.use(express.json());
     this.app.use(cors());
+    this.app.use(express.urlencoded({ extended: true }));
   };
+
+  templates() {
+    this.app.set('view engine', 'pug');
+    this.app.set('views', path.join(__dirname + './../views'))
+  }
 
   router() {
     this.app.use('/v1/productos', router);
@@ -32,17 +40,34 @@ class Server {
   socket() {
     this.io.on('connection', (socket) => {
       console.log('User connected');
+
+      const data = fs.promises.readFile('data/productos.json', 'utf-8');
+      
+      const productos = JSON.stringify(data);
+      
+      socket.emit('productos', productos);
+
+      socket.on('nuevo_producto', producto => {
+
+        productos.push(producto);
+        
+        this.io.emit('productos', productos)
+      })
     });
   }
+  
+  //TODO: Realizar el ultimo paso, que es guardar en el json el nuevo producto
+  //TODO: Realizar un controlador que se encargue de toda la logica del socket
+  //TODO: Realizar el chat entre el cliente y el servidor
 
   listen() {
     this.server.listen(this.port,
       (this.port)
-        ? console.log(`Server running on http://localhost:${this.port}/v1`)
+        ? console.log(`Server running on http://localhost:${this.port}/v1/productos`)
         : console.log('Server Faild, please contact with administrator')
     );
   };
 };
 
 
-export default Server;
+module.exports = Server;
