c2ac4deadd9c651c736cb794e8045d0d3abf07e6
leandroquiroga <leandroquiroga9514@gmail.com>
2022-11-05 21:08:53 -0300

    change: Has been changed type from module to commonjs

--- desafio_seis/controllers/index.js
+++ desafio_seis/controllers/index.js
@@ -1,13 +1,12 @@
-import path from 'path';
-import { fileURLToPath } from 'url'
+const { request, response } = require('express');
+const productos = require('../data/productos.json')
+const pageInitController = (_req = request, res = response, socket) => {
+  res.render('inicio', {
+    page: 'Inicio',
+    subtitle:'Productos',
+    title: 'Cargar producto',
+    productos
+  })
+};
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-console.log(__dirname);
-console.log(__filename);
-const pageInitController = (socket) => {
-  console.log(socket.id);
-}
-
-export default pageInitController;
+module.exports = pageInitController;
