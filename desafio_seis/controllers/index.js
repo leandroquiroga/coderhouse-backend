f66f606954a2e815b1a92ad682b9574a61357e40
leandroquiroga <leandroquiroga9514@gmail.com>
2022-11-10 16:53:45 -0300

    fix: Has been fixed a bug

--- desafio_seis/controllers/index.js
+++ desafio_seis/controllers/index.js
@@ -1,11 +1,13 @@
 const { request, response } = require('express');
-const productos = require('../data/productos.json')
-const pageInitController = (_req = request, res = response, socket) => {
+const products = require('../models/productos.js')
+
+const pageInitController = async (_req = request, res = response, socket) => {
+
   res.render('inicio', {
     page: 'Inicio',
     subtitle:'Productos',
     title: 'Cargar producto',
-    productos
+    allProducts: await products.getAllProducts()
   })
 };
 
