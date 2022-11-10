28d42ae44e11ded42e53250cfd61a05098be6736
leandroquiroga <leandroquiroga9514@gmail.com>
2022-11-10 16:58:23 -0300

    refactor: Has been refactored this script

--- desafio_seis/views/js/index.js
+++ desafio_seis/views/js/index.js
@@ -1,29 +1,46 @@
-const socket = io.connect();
 
 
-socket.on('productos', (productos) => {
+const socket = io();
+const button = document.querySelector('#enviar');
 
-  console.log(productos);
+socket.on('all_products', () => {});
 
-  const button = document.querySelector('#enviar');
-  // Cuando clickeamos el boton
-  button.addEventListener('click', (e) => {
-    e.preventDefault();
 
-    // Obtenemos los valores
-    let name = document.querySelector('#name').value;
-    let price = document.querySelector('#price').value;
-    let url_imagen = document.querySelector('#url_image').value;
-    let mensaje_info = document.querySelector('#mensaje');
+// Cuando clickeamos el boton
+button.addEventListener('click', (e) => {
+  e.preventDefault();
 
-    // Armamos el nuevo producto con un id
-    const nuevoProducto = {
-      name,
-      price,
-      url_imagen,
-      user: socket.id
-    };
+  // Obtenemos los valores
+  let name = document.querySelector('#name');
+  let price = document.querySelector('#price');
+  let url_imagen = document.querySelector('#url_image');
+
+  // Armamos el nuevo producto con un id
+  const nuevoProducto = {
+    name: name.value,
+    price: price.value,
+    url_imagen: url_imagen.value,
+    user: socket.id
+  };
+
+  socket.emit('new_product', nuevoProducto, (data) => {
     
-    socket.emit('nuevo_producto', nuevoProducto)
+    let alertMessage;
+    const { message, status } = data;
+
+    if (!status) {
+      alertMessage = message;
+      alert(alertMessage)
+      return;
+    }
+    alertMessage = message
+    alert(alertMessage);
+    // Obtenemos la url altual
+    const url = window.location.href;
+
+    // Lo hago para que se renderize la pagina
+    window.location.href = url;
   });
-})
+
+  
+});
