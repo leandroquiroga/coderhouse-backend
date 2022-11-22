560624f8ba206b5510286cab11cb553853b3f7a2
leequiro <leandroemanuel.quiroga@nttdata.com>
2022-11-21 21:38:39 -0300

    config: Configuration of the enviroments variable

--- desafio-siete/configuration/enviroment.js
+++ desafio-siete/configuration/enviroment.js
@@ -1,9 +1,9 @@
 const environment = {
   PORT        : process.env.PORT || 3000,
   SQL_HOST    : process.env.SQL_SERVER || 'localhost',
-  SQL_PORT    : process.env.SQL_PORT || 3306,
+  SQL_PORT    : process.env.SQL_PORT || '',
   SQL_USER    : process.env.SQL_USER || 'root',
-  SQL_PASS    : process.env.SQL_PASS || '123456789' ,
+  SQL_PASS    : process.env.SQL_PASS || '' ,
   SQL_DATABASE: process.env.SQL_DATABASE || 'ecommerce' ,
 };
 
