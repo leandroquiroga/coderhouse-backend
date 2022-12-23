e09f3790892df671d6c7ca091b687dca88bb25ac
leandroquiroga <leandroquiroga9514@gmail.com>
2022-12-22 22:51:15 -0300

    route: Has been added a new path - chat

--- desafio-nueve/router/index.js
+++ desafio-nueve/router/index.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
 const { productControllers } = require('../controllers/productControllers');
+const { chatContoller } = require('../controllers/chatContoller');
 
 const router = Router();
 
 router.get('/productos-test', productControllers);
-router.get('/chat', /* chatContoller */);
+router.get('/chat', chatContoller );
 
 
 module.exports = router
