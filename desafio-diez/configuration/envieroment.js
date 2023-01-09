cefd81f673080688c1b665b060737e5c5e81c63f
leandroquiroga <leandroquiroga9514@gmail.com>
2023-01-08 21:00:25 -0300

    new: Has been added a new URL

--- desafio-diez/configuration/envieroment.js
+++ desafio-diez/configuration/envieroment.js
@@ -1,6 +1,7 @@
 const envieroment = {
   URI_PORT: process.env.PORT || '',
-  URI_URL: process.env.URI || ''
+  URI_URL: process.env.URI || '',
+  URI_DB: process.env.URI_DB || '',
 };
 
 module.exports = {
