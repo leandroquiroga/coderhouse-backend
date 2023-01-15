c50d6e99e9d51443846e36e21870e5a4eeb21254
leandroquiroga <leandroquiroga9514@gmail.com>
2023-01-15 17:25:52 -0300

    schema: Schema of Users

---
+++ desafio-once/schemas/users.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require('moongose');
+
+const UserSchema = Schema({
+  name: {
+    type: String,
+    require: true
+  },
+  email: {
+    type: String,
+    require: true
+  },
+  password: {
+    type: String,
+    require: true
+  },
+});
+
+UserSchema.method('toJSON', function () {
+  const { __v, _id, ...user } = this.Object();
+  user.id = _id;
+  return user;
+});
+
+module.exports = model('User', UserSchema);
