d5c6d2a7b8b15a1a0acecfd39aa1ad576cddeef0
leandroquiroga <leandroquiroga9514@gmail.com>
2023-01-15 21:32:37 -0300

    environment: Has been added new URI_DB

--- desafio-once/configuration/environment.js
+++ desafio-once/configuration/environment.js
@@ -1,6 +1,9 @@
 const environment = {
   PORT: process.env.PORT || '',
   URI: process.env.URI || 8000,
+  PASSPORT_SECRET: process.env.PASSPORT_SECRET || '',
+  EXPIRATION_TIME: Number(process.env.EXPIRATION_TIME) || 10000,
+  URI_DB: process.env.URI_DB || ''
 };
 
 module.exports = { environment };
