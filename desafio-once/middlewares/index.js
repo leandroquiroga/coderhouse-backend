d42e8facf296849915b226d36272353b95d4435b
leequiro <leandroemanuel.quiroga@nttdata.com>
2023-01-17 20:50:32 -0300

    middleware: Has been added a new middleware checkAuthPassaportError

--- desafio-once/middlewares/index.js
+++ desafio-once/middlewares/index.js
@@ -1,4 +1,6 @@
 const bCrypr = require('bcrypt');
+const { request } = require('express');
+const passport = require('passport');
 
 const pageNotExist = (req, res, next) => {
   let error = new Error('Recurso no encontrado');
@@ -12,14 +14,15 @@ const errorServer = (error, req, res, next) => {
     error.message = 'Internal Server Error'
   };
 
+  console.log(error);
   res.status(error.status).json({
     message: error.message,
     code: error.status,
   });
 };
 
-const checkAuthentication = (req, res, next) => {
-  
+const checkAuthentication = (req = request, res, next) => {
+
   if (!req.isAuthenticated()) {
     res.redirect('/v1/auth/login');
     return
@@ -27,10 +30,19 @@ const checkAuthentication = (req, res, next) => {
   next()
 };  
 
+const checkAuthPassaportError = (req = request, res, next) => {
+
+  const path = req.path.split('/auth/').slice(1).join('');
+  const pathRedirect = req.path
+
+  passport.authenticate(path, { failureRedirect: `${pathRedirect}` });
+
+  next();
+};
 
 module.exports = {
   pageNotExist,
   errorServer,
   checkAuthentication,
-
+  checkAuthPassaportError,
 }
